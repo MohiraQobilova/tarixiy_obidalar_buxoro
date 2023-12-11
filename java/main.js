ec5b764ffe09e6e41d7035a042a87f365b263e63
MohiraQobilova <139129851+MohiraQobilova@users.noreply.github.com>
2023-12-11 22:16:35 +0400

    hjhjj

---
+++ java/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function mOver(){
+    document.querySelector(".content").style.display="block";
+}
+
+
+
+function click(){
+    document.querySelector(".content").style.display="block";
+}
+
+function closeModal( ){
+    document.querySelector(".content").style.display = "none"
+}   
